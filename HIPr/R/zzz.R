782b34e57b947b76539173a667d607a07beae7ab
Sandra Safo <seaddosafo@gmail.com>
2024-07-08 21:48:10 -0500

    Delete HIPr directory

--- HIPr/R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  # Load the virtual environment if it exists (created with createVirtualenv() function)
-  if(reticulate::virtualenv_exists(envname="HIP_env")){
-    reticulate::use_virtualenv(virtualenv = "HIP_env", required=FALSE)
-  } else{
-    warning("It's recommended you create the package virtual environment with the createVirtualenv() function.")
-  }
-}
